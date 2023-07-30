aac2c1c7e72f3e6825ba65f361d3d783575c392b
BaseApp <133183990+0xBaseApp@users.noreply.github.com>
2023-07-30 17:07:58 +0600

    Add By BaseApp

---
+++ contracts/BaseApp.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BaseApp {
+    string public name = "BaseApp";
+    string public symbol = "BA";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 1000000 * 10 ** uint256(decimals);
+
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(amount <= balances[msg.sender], "ERC20: transfer amount exceeds balance");
+
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowances[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return allowances[owner][spender];
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(amount <= balances[sender], "ERC20: transfer amount exceeds balance");
+        require(amount <= allowances[sender][msg.sender], "ERC20: transfer amount exceeds allowance");
+
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+        allowances[sender][msg.sender] -= amount;
+
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+}
